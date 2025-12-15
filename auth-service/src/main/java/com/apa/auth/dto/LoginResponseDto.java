96ed053566fa902ab0a98370f5c91935bef8fbdd
hyun7494 <et009153@gmail.com>
2025-12-15 23:22:42 +0900

    feat(auth-service): add login dto

---
+++ auth-service/src/main/java/com/apa/auth/dto/LoginResponseDto.java
@@ -0,0 +1,12 @@
+package com.apa.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class LoginResponseDto {
+    private String accessToken;
+    private String tokenType;
+    private long expiresIn;
+}
